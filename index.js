679088c10a50d09b6be4a3a5bf82d76643013fea
thelachu <lachureddykummetha@gmail.com>
2025-09-24 13:30:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function updateTimeDate() {
+  let now = new Date();
+
+  const date = now.toLocaleDateString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    timeZone: "Asia/Kolkata",
+  });
+
+  const time = now.toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Kolkata",
+  });
+
+  document.getElementById("date").textContent = date;
+  document.getElementById("time").textContent = time;
+}
+
+updateTimeDate();
+setInterval(updateTimeDate, 1000);
+
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("ul li");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let value = btn.textContent;
+
+    if (value === "C") {
+      display.value = "";
+    } else if (value === "⌫") {
+      display.value = display.value.slice(0, -1);
+    } else if (value === "=") {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = "Error";
+      }
+    } else {
+      if (value === "x") value = "*";
+      if (value === "÷") value = "/";
+      display.value += value;
+    }
+  });
+});
